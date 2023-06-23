1c1df98f8dd761a108b7ea8d45ddd472e6cb0727
IagoLB <santi_iago@hotmail.com>
2023-06-23 17:26:18 +0200

    Primer commit

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+Estas son prácticas de php
---
+++ variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        //Variables
+       $edad= 33;
+       $sueldo;
+
+       $estatura= 1.75;
+       $nombre="Pepe";
+       $apellido="Pepote";
+       $Profesor=TRUE;
+
+       echo "Tengo ". $edad ." años<br>";
+       echo "Me llamo " . $nombre."<br>";
+       echo $Profesor
+    ?>
+</body>
+</html>
