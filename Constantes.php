ec6e08c8efdb17f43d5a6dccc757cdb90c004697
IagoLB <santi_iago@hotmail.com>
2023-06-24 20:27:44 +0200

    Añadir nuevos archivos

---
+++ Constantes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        const PI = 3.141592;       
+        echo PI;
+
+        echo "<br>";
+
+        define("Dia","hoy");
+        echo Dia
+    ?>
+</body>
+</html>
---
+++ PasoVariables.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="Destino.php?nombre=Pepe&cargo=Jefe"> Click here :)</a>
+    
+</body>
+</html>
