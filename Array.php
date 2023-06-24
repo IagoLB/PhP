727c1f091d0902b67cd41a19ffa253199f485869
IagoLB <santi_iago@hotmail.com>
2023-06-24 21:44:43 +0200

    Añadir nuevos archivos

---
+++ Array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $dias=array("L","M","M","J","V","S","D");
+        $temperatura= [10,20,30,40];
+        $vacio= array(15);
+        $datos= array();
+
+        echo $dias[3];
+        echo "<br>";
+        echo $temperatura[0];
+        echo "<br>";
+
+        var_dump($dias);
+        echo "<br>";
+        var_dump($temperatura);
+    ?>
+</body>
+</html>
